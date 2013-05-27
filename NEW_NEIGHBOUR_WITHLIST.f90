8b1059d29cb95a5346da978aafb59ae879f564b4
Rich <rich@c17>
2013-05-27 13:52:48 +0100

    Fixed error with WITHLIST neighbour method, LCLIST HEAD and CELL must use index_list not universal index

--- NEW_NEIGHBOUR_WITHLIST.f90
+++ NEW_NEIGHBOUR_WITHLIST.f90
@@ -1,4 +1,4 @@
-SUBROUTINE NEW_NEIGHBOUR_WITHLIST (INDEX_LIST,POINT,CELL,LCLIST,N,RLIST,LIST,MAXNAB &
+SUBROUTINE NEW_NEIGHBOUR_WITHLIST(INDEX_LIST,POINT,CELL,LCLIST,N,RLIST,LIST,MAXNAB &
      , MAP, SIZEMAP, HEAD, MAXNUMCELL)
 
   USE VAR
@@ -8,7 +8,7 @@ SUBROUTINE NEW_NEIGHBOUR_WITHLIST (INDEX_LIST,POINT,CELL,LCLIST,N,RLIST,LIST,MAX
   INTEGER, INTENT(IN) :: N, MAXNUMCELL, MAXNAB
   INTEGER, INTENT(IN) :: INDEX_LIST(N), CELL(N), LCLIST(N), SIZEMAP, MAP(SIZEMAP), HEAD(MAXNUMCELL)
   INTEGER, INTENT(INOUT) :: POINT(N+1), LIST(MAXNAB)
-  INTEGER :: A, B, C, I, J, NLIST, JCELL, JCELL0, NABOR
+  INTEGER :: A, B, C, D, I, J, NLIST, JCELL, JCELL0, NABOR
   REAL(KIND=RKIND), INTENT(IN) :: RLIST
   REAL(KIND=RKIND) :: RXI, RYI, RZI
   REAL(KIND=RKIND) :: RIJSQ
@@ -29,25 +29,29 @@ SUBROUTINE NEW_NEIGHBOUR_WITHLIST (INDEX_LIST,POINT,CELL,LCLIST,N,RLIST,LIST,MAX
      RXI = RX(I)
      RYI = RY(I)
      RZI = RZ(I)
-
-     J = LCLIST(A)
+  
+     B = LCLIST(A)
      DO
-        IF(J .eq. 0) EXIT
+        IF(B .eq. 0) EXIT !If reached end of list
+        J = INDEX_LIST(B) !Fetch real index of B
+
         !           if(abs(ip-jp) .le. contactA)then
         NONBOND=1
         IF(CONNECTIONS(I) .gt. 0) THEN !If I has connections:
-           DO B=1,CONNECTIONS(I)
-              C = CONNECTED_TO(I,B)
-              IF(J .eq. C) THEN !If J is C then they are connected
+           DO C=1,CONNECTIONS(I) !Connections is the number of connected atoms to I
+              D = CONNECTED_TO(I,C) !Connected_to has the index of all connected atoms
+              IF(J .eq. D) THEN !If J is D then they are connected
                  NONBOND=0 
                  EXIT
               END IF
            END DO
         END IF
-        !           else
-        !              NONBOND=1
-        !           end if
-        !call NON_BOND_ARRAY(IP,JP)
+        IF(IBRDESCR .eq. 0) THEN
+           IF(I .gt. NATOMS .and. J .gt. NATOMS) THEN !Exclude VS-VS interactions
+              NONBOND = 0
+           END IF
+        END IF
+
         IF (NONBOND.EQ.1) THEN
            RXIJ = RXI - RX(J)
            RYIJ = RYI - RY(J)
@@ -59,42 +63,41 @@ SUBROUTINE NEW_NEIGHBOUR_WITHLIST (INDEX_LIST,POINT,CELL,LCLIST,N,RLIST,LIST,MAX
            RIJSQ = RXIJ**2 + RYIJ**2 + RZIJ**2
 
            IF (RIJSQ.LT.RLISTSQ) THEN
-if (i .gt. natoms) THEN
-   write(222,*)J,VIRT_CENTER(I-NATOMS),rijsq 
-ELSE IF(j .gt. natoms) THEN
-   write(222,*)i,VIRT_CENTER(J-NATOMS),rijsq 
-END IF   
-           NLIST = NLIST + 1
+              NLIST = NLIST + 1
               LIST(NLIST) = J
               IF (NLIST.EQ.MAXNAB) STOP 'LIST TOO SMALL'
            ENDIF
         ENDIF
-        J = LCLIST(A)
+        B = LCLIST(B) !Error is here
      END DO
 
         !***********LOOP IN THE NEIGHBOURING CELLS**********************************
      JCELL0 = 13*(JCELL - 1)
      DO NABOR = 1,13
         JCELL = MAP(JCELL0+NABOR)
-        J = HEAD(JCELL)
+        B = HEAD(JCELL)
 
-        IF(J .eq. 422) WRITE(*,*) 'ciao'
         DO
-           IF(J .eq. 0) EXIT
+           IF(B .eq. 0) EXIT
+           J = INDEX_LIST(B)
            !              if(abs(ip-jp) .le. contactA)then
            !              else
            !                 NONBOND=1
            !              end if
-
            NONBOND=1
            IF(CONNECTIONS(I) .gt. 0) THEN !If I has connections:
-              DO B=1,CONNECTIONS(I)
-                 C = CONNECTED_TO(I,B)
-                 IF(J .eq. C) THEN !If J is C then they are connected
+              DO C=1,CONNECTIONS(I)
+                 D = CONNECTED_TO(I,C)
+                 IF(J .eq. D) THEN !If J is C then they are connected
                     NONBOND=0 
                     EXIT
                  END IF
               END DO
+           END IF
+           IF(IBRDESCR .eq. 0) THEN
+              IF(I .gt. NATOMS .and. J .gt. NATOMS) THEN
+                 NONBOND = 0
+              END IF
            END IF
 
            IF (NONBOND.EQ.1) THEN
@@ -105,21 +108,16 @@ END IF
               RXIJ = RXIJ - ANINT(RXIJ*BOXXINV)*BOXX
               RYIJ = RYIJ - ANINT(RYIJ*BOXYINV)*BOXY
               RZIJ = RZIJ - ANINT(RZIJ*BOXZINV)*BOXZ
-3              RIJSQ = RXIJ**2 + RYIJ**2 + RZIJ**2
+              RIJSQ = RXIJ**2 + RYIJ**2 + RZIJ**2
 
               IF (RIJSQ.LT.RLISTSQ) THEN
-if (i .gt. natoms) THEN
-   write(222,*)J,VIRT_CENTER(I-NATOMS),rijsq 
-ELSE IF(j .gt. natoms) THEN
-   write(222,*)i,VIRT_CENTER(J-NATOMS),rijsq 
-END IF   
-
                  NLIST = NLIST + 1
                  LIST(NLIST) = J
                  IF (NLIST.EQ.MAXNAB) STOP 'LIST TOO SMALL'
               ENDIF
+
            ENDIF
-           J = LCLIST(A)
+           B = LCLIST(B)
         END DO
      END DO
 
